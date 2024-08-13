8ee5e9185744492f56bdc6e8a95d121f6b6ed12f
17673 <1767391352@qq.com>
2024-08-13 14:09:11 +0800

    update

---
+++ cif2VASP/phono.py
@@ -0,0 +1,3 @@
+import phonopy
+from phonopy.phonon.band_structure import get_band_qpoints_and_path_connections
+
---
+++ npy/npy.py
@@ -0,0 +1,10 @@
+import numpy as np
+from PIL import Image
+# 打开npy文件
+
+if __name__ == '__main__':
+
+# 查看数据
+    data = np.load('./sub_b_dos_300K.npy', allow_pickle=True)
+
+    print(data[""])
